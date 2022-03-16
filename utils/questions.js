f0e80ea84ba52a17ac7810a1cfdc70961acfa89c
Maryia <malakhava@yahoo.com>
2022-03-16 01:15:11 -0400

    add options

--- utils/questions.js
+++ utils/questions.js
@@ -47,5 +47,73 @@ module.exports = {
 		},
 	],
 
+	// Option to add employee
+	insertEmployee: (departmentArray, roleArray, managerArray) => [
+		// Create Employee's First Name
+		{
+			name: "firstName",
+			type: "input",
+			message: "Enter employee's first name:",
+		},
+		// Create Employee's Last Name
+		{
+			name: "lastName",
+			type: "input",
+			message: "Enter employee's last name:",
+		},
+		// Select Employee's Department
+		{
+			name: "department",
+			type: "list",
+			message: "Choose employee's department",
+			choices: departmentArray,
+		},
+		// Select Employee's Role
+		{
+			name: "role",
+			type: "list",
+			message: "Choose employee's job position",
+			choices: roleArray,
+		},
+		// Select Employee's Manager
+		{
+			name: "manager",
+			type: "list",
+			message: "Choose the manager of this employee:",
+			choices: managerArray,
+		},
+	],
+
+	// Option to add department
+	insertDepartment: {
+		// Create New Departments Name
+		name: "department",
+		type: "input",
+		message: "What is the name of the new department?",
+	},
+
+	// Option to add role
+	insertRole: (departmentChoices) => [
+		// Create New Role's Name
+		{
+			type: "input",
+			name: "roleTitle",
+			message: "Role title?",
+		},
+		// Create New Role's Salary Budget
+		{
+			type: "input",
+			name: "roleSalary",
+			message: "Role Salary",
+		},
+		// Select New Role's Department
+		{
+			type: "list",
+			name: "departmentId",
+			message: "Department?",
+			choices: departmentChoices,
+		},
+	],
+
 		
 };
